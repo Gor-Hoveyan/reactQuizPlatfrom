022297370d2175bae6753d82c55e34eaec6f8227
Gor-Hoveyan <gorpro2004@mail.ru>
2025-09-19 23:01:52 +0400

    Build issue fix

--- client/src/components/addPostBtn/AddPostBtn.tsx
+++ client/src/components/addPostBtn/AddPostBtn.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import styles from './AddPostBtn.module.scss';
-import { NavLink } from 'react-router-dom';
-import { FiPlusCircle } from 'react-icons/fi';
+import React from "react";
+import styles from "./AddPostBtn.module.scss";
+import { NavLink } from "react-router-dom";
+import { FiPlusCircle } from "react-icons/fi";
 
 interface IProps {
-    postType: 'test' | 'quiz'
+  postType: "test" | "quiz";
 }
 
-export default function AddPostBtn({postType}: IProps) {
-    return (
-        <div className={styles.addPost}>
-                <NavLink to={`/${postType}/create`}><FiPlusCircle size={70} cursor={'pointer'} color='gray'
-                    onMouseMove={(target) => target.currentTarget.style.color = 'black'}
-                    onMouseOut={(target) => target.currentTarget.style.color = 'gray'}
-                    style={{ transitionDuration: '0.4s' }} />
-                </NavLink>
-            </div>
-    );
+export default function AddPostBtn({ postType }: IProps) {
+  return (
+    <div className={styles.addPost}>
+      <NavLink
+        to={`/${postType}/create`}
+        color="gray"
+        onMouseMove={(target) => (target.currentTarget.style.color = "black")}
+        onMouseOut={(target) => (target.currentTarget.style.color = "gray")}
+        style={{ transitionDuration: "0.4s" }}
+      >
+        <FiPlusCircle size={70} />
+      </NavLink>
+    </div>
+  );
 }
